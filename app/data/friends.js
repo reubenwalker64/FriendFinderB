2f2890fe611d7d9df7b2d91dd446bd087e19d15f
reubenwalker64 <rwalker@mobile-atom.com>
2019-03-02 10:44:43 -0500

    fix

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+/* You should save your application's data inside of `app/data/friends.js` as an array of objects.
+Each of these objects should roughly follow the format below.
+
+```json
+{
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+```
+EDIT Below
+// DATA
+// Below data will hold all of the waitlist tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var waitingArray = [
+  {
+    customerName: "Saima",
+    customerEmail: "saima@example.com",
+    phoneNumber: "000-000-0000",
+    customerID: "saimaCool"
+  }
+];
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = waitingArray;
+
+*/
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,54 @@
+/*Your `htmlRoutes.js` file should include two routes:
+
+   * A GET Route to `/survey` which should display the survey page.
+   * A default, catch-all route that leads to `home.html` which displays the home page.
+
+*/
+
+/*
+6. Determine the user's most compatible friend using the following as a guide:
+
+   * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+   * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
+     * Example:
+       * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+       * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+       * Total Difference: **2 + 1 + 2 =** **_5_**
+   * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
+   * The closest match will be the user with the least amount of difference.
+
+7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+   * The modal should display both the name and picture of the closest match.
+
+EDIT BELOW */
+
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/tables.html"));
+  });
+
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
