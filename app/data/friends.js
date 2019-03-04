84457121ecb9dcae714e874c5c60ac7fa1c88d8a
Reuben Walker, Jr <rwalker@mobile-atom.com>
2019-03-04 12:28:21 -0500

    Add files via upload

--- app/data/friends.js
+++ app/data/friends.js
@@ -25,18 +25,99 @@ EDIT Below
 // Initially we just set it equal to a "dummy" customer.
 // But you could have it be an empty array as well.
 // ===============================================================================
-
-var waitingArray = [
+*/
+var friends = [
   {
-    customerName: "Saima",
-    customerEmail: "saima@example.com",
-    phoneNumber: "000-000-0000",
-    customerID: "saimaCool"
+    name: "Alice Springs",
+    photo: "../images/alice-springs.png",
+    scores: [5,
+		1,
+		4,
+		4,
+		5,
+		1,
+		2,
+		5,
+		4,
+		1]
+  }
+
+	{
+    name: "Eric Red",
+    photo: "../images/eric-red.png",
+    scores: [1,
+		5,
+		2,
+		2,
+		1,
+		5,
+		4,
+		1,
+		2,
+		1]
+  }
+
+	{
+    name: "India Faux",
+    photo: "../images/india-faux.png",
+    scores: [2,
+		4,
+		1,
+	  1,
+		2,
+		4,
+		5,
+		2,
+		1,
+		5]
+  }
+
+	{
+    name: "Jean Iris",
+    photo: "../images/jean-ris.png",
+    scores: [4,
+		2,
+		5,
+		5,
+		4,
+		2,
+		1,
+		4,
+		5,
+		2]
+  }
+
+	{
+		name: "Jeff Painter",
+		photo: "../images/jeff-painter.png",
+		scores: [2,
+		4,
+		1,
+		1,
+		2,
+		2,
+		1,
+		4,
+		5,
+		2]
+	}
+
+	{
+    name: "MC Jones",
+    photo: "../images/mc-jones.png",
+    scores: [4,
+		2,
+		2,
+		2,
+		4,
+		2,
+		4,
+		4,
+		2,
+		2]
   }
 ];
 
 
 // Note how we export the array. This makes it accessible to other files using require.
-module.exports = waitingArray;
-
-*/
+module.exports = friends;
